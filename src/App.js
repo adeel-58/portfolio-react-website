97c8437d423608a23f076936a352f88be22ef1e5
adeel-58 <khuffia58@gmail.com>
2025-08-17 15:42:34 +0300

    this is the clean inital website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World 🚀</h1>
+      <p>Welcome to your new React app!</p>
+    </div>
+  );
+}
+
+export default App;
